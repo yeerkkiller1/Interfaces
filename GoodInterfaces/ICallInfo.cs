4a9b8de19c2d05e5db72511d6cb3e55382b3a4be
Quentin Brooks <yeerkkiller1@gmail.com>
2015-01-24 20:26:55 -0800

    idk

---
+++ GoodInterfaces/ICallInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodInterfaces
+{
+    public interface ICallInfo : IComparable<ICallInfo>
+    {
+        string MemberName { get; }
+        string SourceFilePath { get; }
+        int LineNumber { get; }
+    }
+}
---
+++ GoodInterfaces/IObserv.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodInterfaces
+{
+    interface IObserv<T>
+    {
+        bool HasBeenSet { get; }
+        T Get();
+        void Set(T newValue);
+        /// <param name="callIfSet"> This means, if HasBeenSet, we will call your handler. Basically: 
+        /// int id = x.Subscribe(handler, false); if(x.HasBeenSet) handler(x.Get(), id);
+        /// ==
+        /// x.Subscribe(handler, true)</param>
+        /// <returns>Unique id (probably, until we wrap around the integer :P) that canb e used to destroy the observable.</returns>
+        int Subscribe(Action<T> handler, bool callIfSet = true);
+        void Unsubscribe(int id);
+    }
+}

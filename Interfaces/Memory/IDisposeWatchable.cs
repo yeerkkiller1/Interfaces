d203ce1a46bce810d85926c8a1a06c2eab3905a0
Quentin Brooks <yeerkkiller1@gmail.com>
2015-01-24 22:10:11 -0800

    IDiposeWatchable should be in Interfaces

---
+++ Interfaces/Memory/IDisposeWatchable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodInterfaces.Memory
+{
+    public interface IDisposeWatchable : IDisposable
+    {
+        int OnDispose(Action action);
+        void OffDispose(int id);
+        bool IsDisposed();
+    }
+}

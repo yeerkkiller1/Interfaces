84b1bdb3eab561f5598b6d2c0c41a318fa3192a7
Quentin Brooks <yeerkkiller1@gmail.com>
2015-01-24 21:48:52 -0800

    Interfaces

--- Interfaces/ICallInfo.cs
+++ Interfaces/ICallInfo.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GoodInterfaces
+namespace Interfaces
 {
     public interface ICallInfo : IComparable<ICallInfo>
     {
